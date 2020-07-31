d89181ec16e9f2c5976f8e918789a2a769e1e6c3
Monique.Bass01 <monique.bass01@gmail.com>
2020-07-31 11:16:41 -0500

    Add project files.

---
+++ ConsoleInput/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Mask 
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+             Console.WriteLine("Enter in a secret.");
+            var mysecret = Console.ReadLine();
+            string hiddenletters = "";
+
+            mysecret.ToCharArray();
+            for (int i = 0; i < mysecret.Length; i++)
+            {
+                if (mysecret.Length - 4 > i)
+                {
+                    hiddenletters += "*";
+                } else
+                {
+                    hiddenletters += mysecret[i];
+                }
+            }
+            Console.WriteLine(hiddenletters);
+            Console.Read();
+        }
+    }
+}
